8a3b513425001706283b4d158f7bb18277967399
Ernesto Borio <ernestoborio@gmail.com>
2019-10-14 23:26:10 -0300

    no message

--- io.go
+++ io.go
@@ -1,7 +1,7 @@
 package c64
 
-/**
-	Handles all writes and reads in the I/O memory mapped registers $D000..$DFFF
+/*
+Handles all writes and reads in the I/O memory mapped registers $D000..$DFFF
 */
 
 // Read I/O ports
@@ -11,5 +11,5 @@ func (c64 *C64) readIO(address uint16) byte {
 
 // Write I/O ports
 func (c64 *C64) writeIO(address uint16, value byte) {
-	
+
 }
